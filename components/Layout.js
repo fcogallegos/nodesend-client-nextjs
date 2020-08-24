4ef1031c503d4c5625ea1feb111cf6cf9221eda8
Francisco Gallegos <fgallegosgo@gmail.com>
2020-08-24 14:25:41 -0600

    creating the Header Component

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-
+import Header from './Header';
 
 const Layout = ({children}) => {
     return ( 
@@ -11,10 +11,9 @@ const Layout = ({children}) => {
 
             </Head>
 
-            <img src="logo.svg" />
-
             <div className="bg-gray-100 min-h-screen">
                 <div className="container mx-auto">
+                    <Header />
                     <main className="mt-20">
                         {children}
                     </main>                    
