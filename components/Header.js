72ac34da585219e1fb4f57af73db2238da800b16
Francisco Gallegos <fgallegosgo@gmail.com>
2020-08-26 10:31:17 -0600

    Routing to Login and Create Account

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Link from 'next/link';
 
 
 const Header = () => {
     return ( 
         <header className="py-8 flex flex-col md:flex-row items-center justify-between">
-            <img className="w-64 mb-8 md:mb-0" src="logo.svg" />
+            <Link href="/">
+                <img className="w-64 mb-8 md:mb-0" style={{ cursor: `pointer` }} src="logo.svg" />
+            </Link>
+            
+
+            <div>
+                <Link href="/login">
+                    <a className="bg-red-500 px-5 py-3 rounded-lg text-white font-bold uppercase mr-2">Log in</a>
+                </Link>
+                <Link href="/createaccount">
+                    <a className="bg-black px-5 py-3 rounded-lg text-white font-bold uppercase">Create Account</a>
+                </Link>
+            </div>
         </header>
      );
 }
