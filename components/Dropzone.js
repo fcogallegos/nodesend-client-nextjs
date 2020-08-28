650615c3024b1d6d6c22a20223204fc48536f38c
Francisco Gallegos <fgallegosgo@gmail.com>
2020-08-28 14:34:05 -0600

    adding React Dropzone

--- components/Dropzone.js
+++ components/Dropzone.js
@@ -4,9 +4,26 @@ import clientAxios from '../config/axios';
 
 
 const Dropzone = () => {
+
+    // extract content of Dropzone
+    const { getRootProps, getInputProps, isDragActive } = useDropzone();
+
     return ( 
         <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
-            <p>Dropzone here</p>
+            <div {...getRootProps({ className: 'dropzone w-full py-32' })} >
+                <input className="h-100" {...getInputProps()}  />
+
+                
+                    {
+                        isDragActive ? <p className="text-2xl text-center text-gray-600">Drop the file</p> :
+                            <div className="text-center">
+                                <p className="text-2xl text-center text-gray-600">Select a file or drag here</p>
+                                <button className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800" type="button">
+                                    Select a file to upload
+                                </button>
+                            </div>
+                        }
+            </div>
         </div>
      );
 }
