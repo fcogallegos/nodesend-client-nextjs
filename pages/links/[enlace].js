68f2c896c33e92007ba04979c0b9efd033471d48
Francisco Gallegos <fgallegosgo@gmail.com>
2020-09-02 16:59:53 -0600

    downloading the files associates to URL

--- pages/links/[enlace].js
+++ pages/links/[enlace].js
@@ -34,7 +34,14 @@ export default ({enlace}) => {
     console.log(enlace);
     return (
         <Layout>
-            <h1>from [link].js</h1>
+            <h1 className="text-4xl text-center text-gray-700">Download your file</h1>
+            <div className="flex items-center justify-center mt-10">
+                <a 
+                    href={`${process.env.backendURL}/api/files/${enlace.file}`} 
+                    className="bg-red-500 text-center px-10 py-3 rounded uppercase font-bold text-white cursor-pointer"
+                    download
+                >Here</a>
+            </div>
         </Layout>
     )
 }
