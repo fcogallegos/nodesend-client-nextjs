1cb419766a226cccd588e836848e14c9f0e02cf9
Francisco Gallegos <fgallegosgo@gmail.com>
2020-09-04 17:11:12 -0600

    showing a Form for Passwords

--- components/Form.js
+++ components/Form.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+
+    const [ hasPassword, setHasPassword ] = useState(false);
+
     return ( 
         <div className="w-full mt-20">
             <div>
@@ -12,6 +15,23 @@ const Form = () => {
                     <option value="10">Ten Downloads</option>
                     <option value="20">Twenty Downloads</option>
                 </select>
+            </div>
+            <div className="mt-5">
+                <div className="flex justify-between items-center">
+                    <label className="text-lg text-gray-800 mr-2">Password Protect</label>
+                    <input 
+                        type="checkbox" 
+                        onChange={() => setHasPassword(!hasPassword) }
+                    />
+                    
+                </div>
+                { hasPassword ? (
+                    <input 
+                        type="password" 
+                        className="appearance-none w-full mt-2 bg-white border border-gray-400 text-black py-3 px-4 pr-8 rounded leading-none focus:outline-none focus:border-gray-500" 
+                    />
+                ) : null }
+                
             </div>
         </div>
      );
